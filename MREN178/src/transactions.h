d551668d9d32f126a5fa52c0a0bbb71353e124c3
epicboats <junonmoy@gmail.com>
2026-04-01 10:27:54 -0400

    Add logging functions to transactions header

--- MREN178/src/transactions.h
+++ MREN178/src/transactions.h
@@ -4,5 +4,7 @@
 void processRestock(int pid, int loc, int qty);
 void processShip(int pid, int loc, int qty);
 void processTransfer(int pid, int from, int to, int qty);
+void logTransaction(const char* entry);
+void saveTransactionLog(const char* filename);
 
 #endif
