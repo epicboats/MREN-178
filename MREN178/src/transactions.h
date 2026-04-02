c869145f01e9cd2ced46f724bec93e12bbb91210
epicboats <junonmoy@gmail.com>
2026-04-02 18:48:44 -0400

    Update transactions.h

--- MREN178/src/transactions.h
+++ MREN178/src/transactions.h
@@ -1,10 +1,5 @@
-#ifndef TRANSACTIONS_H
-#define TRANSACTIONS_H
-
 void processRestock(int pid, int loc, int qty);
 void processShip(int pid, int loc, int qty);
 void processTransfer(int pid, int from, int to, int qty);
 void logTransaction(const char* entry);
 void saveTransactionLog(const char* filename);
-
-#endif
